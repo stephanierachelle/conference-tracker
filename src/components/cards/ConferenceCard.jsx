58954272292254e26fdefeea724d70bbb4419bdf
Steph <stephaniergreene94@gmail.com>
2019-05-03 00:08:05 +1000

    UI complete

--- src/components/cards/ConferenceCard.jsx
+++ src/components/cards/ConferenceCard.jsx
@@ -1,17 +1,19 @@
 const m = require("mithril");
 
+import ConferenceField from "./ConferenceField.jsx";
+
 const ConferenceCard = {
     view: ({ attrs }) => (
     <div class="conference-card">
-        <div class="conference-fileds">
-            <span>
-            {attrs.conference.name} @ {attrs.conference/location}
-            </span>
-                <i class="fas fa-star" />
+        <div class="conference-fields">
+            <ConferenceField
+            fieldValue={`${attrs.conference.name} @ ${attrs.conference.location}`}
+            />
+            <ConferenceField fieldValue={<i class="fas fa-star" />} />
             </div>
             <div class ="conference-fields">
-            <span>{attrs.conference.date}</span>
-            <span>{`19 d 20 hr 45m`}</span>
+            <ConferenceField fieldValue={attrs.conference.date} />
+            <ConferenceField fieldValue={`19 d 20 hr 45m`} />
         </div>
     </div>
     )
---
+++ src/components/cards/ConferenceField.jsx
@@ -0,0 +1,7 @@
+const m = require("mithril");
+
+const ConferenceField = {
+    view: ({ attrs }) => <div class="conference-field">{attrs.fieldValue}</div>
+};
+
+export default ConferenceField;
