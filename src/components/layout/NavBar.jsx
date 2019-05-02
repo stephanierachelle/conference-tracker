60cd6655f9eeda045321a59c75d26aeb84d82235
Steph <stephaniergreene94@gmail.com>
2019-05-02 22:59:46 +1000

    navButton completed

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,7 +1,15 @@
 const m = require("mithril");
 
+import NavButton from "../ui/NavButton.jsx";
+
 const NavBar = {
-    view: () => <div class="nav-bar"/>
+    view: () => (
+    <div class="nav-bar">
+        <NavButton path={`cfp`} icon={<i class="fas fa-microphone" />} />
+        <NavButton path={`conferences`} icon={<i class="fas fa-users" />} />
+        <NavButton path={`entry`} icon={<i class="fas fa-edit" />} />
+    </div>
+    )
 };
 
 export default NavBar;
