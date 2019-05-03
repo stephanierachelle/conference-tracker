380a23b8a6b68935f53e6d588f43f240748ea3b9
Steph <stephaniergreene94@gmail.com>
2019-05-03 20:19:43 +1000

    start of favourites icon toggle

---
+++ src/components/cards/FavouriteConference.jsx
@@ -0,0 +1,20 @@
+const m = require("mithril");
+
+import ConferenceCard from "./ConferenceCard";
+
+//toogle hide/show the element "fas fa-star"
+
+const FavouriteConference ={
+    view: ({ attrs }) => (
+        <div class="fas fa-star">
+
+        <div class ></div>
+    )
+        
+}
+export default FavouriteConference;
+
+//onclick star
+
+
+//add to favourites
---
+++ src/services/auth0-variables.js
@@ -0,0 +1,10 @@
+//src/servicecs/auth0-variables.js
+
+const AUTH0 = {
+    CLIENTID:"<your client id>",
+    DOMAIN:"<your domain>",
+    CALLBACKURL: "<your callback URL>",
+    AUDIENCE: "https://<your domain>/userinfo"
+};
+
+export default AUTH0;
