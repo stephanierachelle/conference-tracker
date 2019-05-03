c56ef74a006bbdd5c8e56af772abfcc7e38e637e
Steph <stephaniergreene94@gmail.com>
2019-05-03 12:37:11 +1000

    applied mithril state management to the (Call papers to Deadlaine) input field, yes + no reaction

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -2,25 +2,58 @@ const m = require("mithril");
 
 import UIButton from "./ui/UIButton.jsx";
 
-const EntryFrom = {
+const EntryForm = {
+     data: {        //State of EntryForm component
+        CFP: false
+    },
     view: vnode => (
         <form name="entry-form" id="entry-form">
-        <label for="conf-name">{`Conference Name`}</label>
-        <input id="conf-name" type="text" name="name" />
-        <label for="location">{`Location`}</label>
-        <input id="location" type="text" name="location" />
-        <label for="date">{`Date`}</label>
-        <input id="date" type="text" name="date" />
+        {/* ... */}
+      <label for="conf-name">
+        {`Conference Name`}
+      </label>
+      <input id="conf-name" type="text" name="name" />
+      <label for="location">
+        {`Location`}
+      </label>
+      <input id="location" type="text" name="location" />
+      <label for="date">
+        {`Date`}
+      </label>
+      <input id="date" type="text" name="date" />
+
         <label class="form-question">
         {`submitting paper?`}
         <label for="yes-cfp">Yes</label>
-        <input value={true} type="radio" id="yes-cfp" name="CFP" />
+        <input 
+        value={true} 
+        type="radio" 
+        id="yes-cfp" 
+        name="CFP" 
+        onclick={() => {
+            vnode.state.CFP = true;
+        }}
+        />
+        <label for="no-cfp">No</label>
+        <input
+            value={false}
+            type="radio"
+            id="no-cfp"
+            name="CFP"
+            onclick={() => {
+                vnode.state.CFP = false;  
+        }}
+        />
         </label>
-        <label for='cfp'>{`Call for Papers Dealine`}</label>,
-        <input id="cfp" type="text" name="CFPDate" />
-        <UIButton action={() => console.log(`Saving...`)} buttonName="SAVE" />
+        {vnode.state.CFP
+            ? [
+            <label for="cfp">{`Call for Papers Dealine`}</label>,
+            <input id="cfp" type="text" name="CFPDate" />
+            ]
+        : null}
+     <UIButton action={() => console.log(`Saving...`)} buttonName="SAVE" />
         </form>
     )
 };
 
-export default EntryFrom;
+export default EntryForm;
